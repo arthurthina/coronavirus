30573a95d0717778c6007804ee2fc49adb6a5ab9
Arthur Thina <arthur.thina@gmail.com>
2020-03-25 19:56:06 -0300

    log10 y

--- coronavirus_brasil.R
+++ coronavirus_brasil.R
@@ -1,6 +1,7 @@
 library(nCov2019)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
 
 y <- load_nCov2019(lang = 'en', source='github')
 corona <- y['global']
@@ -8,8 +9,15 @@ corona <- y['global']
 
 br <- corona %>%
   filter(country == "Brazil" | country == "Italy" | country == "Iran" | country == "China" | country == "United States"
-         | country == "Spain" | country == "Japan")
+         | country == "Spain" | country == "Japan" | country == "France")
   
+
+t <- corona %>% 
+  distinct(country, .keep_all = TRUE) %>% 
+  arrange(desc(time))
+t
+
+
 br_plot <- ggplot(br, aes(x = time, y = log(cum_confirm), col = country)) +
   geom_point() +
   geom_line() 
