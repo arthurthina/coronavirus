9ef1cfd1726bea273677ca6c8a45319622cec929
Arthur Thina <arthur.thina@gmail.com>
2020-04-02 01:11:19 -0300

    color brewer

--- coronavirus_brasil.R
+++ coronavirus_brasil.R
@@ -2,6 +2,7 @@ library(nCov2019)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(RColorBrewer)
 
 y <- load_nCov2019(lang = 'en', source = 'github')
 corona <- y['global']
@@ -16,6 +17,8 @@ head(br)
 br_plot <- ggplot(br, aes(x = time, y = cum_confirm, col = country)) +
   geom_point() +
   geom_line() +
-  scale_y_log10()
+  scale_y_log10() +
+  scale_colour_brewer(palette = "Spectral") +
+  theme_minimal()
 
 br_plot
