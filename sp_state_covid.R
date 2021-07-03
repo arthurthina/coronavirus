731059d7749d5ce364275391ddbb1b4840bf61f3
Arthur Thina <arthur.thina@gmail.com>
2021-07-03 03:21:35 -0300

    initial commit

---
+++ sp_state_covid.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(readxl)
+library(RColorBrewer)
+library(ggthemr)
+library(lubridate)
+
+states <- read_xlsx("data_covid_state/states.xlsx", 
+                     col_types = c("text", "text", "numeric", 
+                                   "text", "text", "text", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "numeric", "numeric", "numeric", 
+                                   "text")) %>% 
+  hablar::retype()
+
+states[is.na(states)] <- 0
+states$age_group <- factor(states$age_group, levels = c("9-", "10-19", "20-29", "30-39", "40-49", "50-59", 
+                                                           "60-69", "70-79", "80-89", "90-99", "100+", "NA"  ))
+
+levels(states$age_group)
+
+states %>% 
+  filter(state == "SP" & age_group == "9-")
+summary(df_sp)
+
+df_sp <- states %>% 
+  filter(state == "SP" & date >= "2020-03-10")
+
+tail(df_sp$date)
+unique(df_sp$age_group)
+
+ggthemr::ggthemr('earth', type = 'outer')
+ggplot(df_sp, aes(x = date, y = deaths_covid19, color = age_group)) +
+  geom_point() +
+  geom_jitter() +
+  scale_x_date(date_labels = "%m-%Y", date_breaks = "1 month") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(x = "Date",
+       y = "Deaths") +
+ facet_wrap(~ age_group)
+
+
+df_sp_month <- df_sp %>% 
+  group_by(month = floor_date(date, unit = "month"))
+
+ggplot(df_sp_month, aes(x = date, y = deaths_covid19, color = age_group)) + 
+  geom_col() + 
+  scale_x_date(date_labels = "%b %y", breaks = "month")
